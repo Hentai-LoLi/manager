a0c9f8bb3fb7b8ee3609e68ada85550b749f66c5
Hentai_Loli <1653728484@qq.com>
2020-11-05 10:01:12 +0800

    完成了RBAC方面的控制

--- src/main/java/com/key/ManagerApplication.java
+++ src/main/java/com/key/ManagerApplication.java
@@ -1,9 +1,11 @@
 package com.key;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.key.mapper")
 public class ManagerApplication {
 
     public static void main(String[] args) {
